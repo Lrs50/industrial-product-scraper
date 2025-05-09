e528ff5fd12bb1ba8ca1d6945ec1dd9fc7836696
lrs50 <lucaspook12@gmail.com>
2025-05-09 19:47:28 -0300

    add: parser performance

--- src/scraper/parser.py
+++ src/scraper/parser.py
@@ -117,19 +117,10 @@ class Parser(object):
         if not specs_div:
             self.logger.warning("Specs div not found")
         
-        columns = specs_div.find_all("div", class_="col")
-        
-        for col in columns:
-            for item in col.find_all("div",recursive=False):
-                label = item.find("span",class_="label")
-                value = item.find("span",class_="value")
-                
-                if label and value:
-                    key = label.get_text(strip=True)
-                    val = value.get_text(separator=", ",strip=True)
-                    specs[key] = val
+        specs = self.parse_label_value_grid(specs_div)
         
         self.logger.info("Sucessfully retrieved Specs info")    
+        
         return specs   
     
     def parse_nameplate(self,soup):
@@ -165,13 +156,95 @@ class Parser(object):
             nameplate["EXTRAS"] = extras
             
         self.logger.info("Sucessfully retrieved nameplate info") 
+        
         return nameplate
+     
     
+    def parse_label_value_grid(self,table):
+        data = {}
+        columns = table.find_all("div", class_="col")
+    
+        for col in columns:
+            for item in col.find_all("div",recursive=False):
+                label = item.find("span",class_="label")
+                value = item.find("span",class_="value")
+                
+                if label and value:
+                    key = label.get_text(strip=True)
+                    val = value.get_text(separator=", ",strip=True)
+                    data[key] = val
+    
+        return data
+        
     def parse_performance(self,soup):
         
         performance = {}
         
+        performance_div = soup.find("div", class_="pane", attrs={"data-tab": "performance"})
+        
+        try:
+            description = [
+                h2.get_text(strip=True)
+                for h2 in performance_div.find_all("h2")
+                if not h2.find_parent("div", class_="tabHeading")][0]
+            
+            performance["description"] = description
+        except:
+            self.logger.warning("No description found in Performance")
+
+        try:
+            observation = performance_div.find("em").get_text(strip=True)
+            performance["observation"] = observation
+        except:
+            self.logger.warning("No observation found in Performance")
+        
+        h3_tags = [h3.get_text(strip=True).lower() for h3 in performance_div.find_all("h3")]
+        
+        if "general characteristics" in h3_tags:
+
+            table = performance_div.find("div", class_="product-overview")                        
+            performance["general_characteristics"] = self.parse_label_value_grid(table) 
+            
+        if "load characteristics" in h3_tags:
+            data = {}
+            table = performance_div.find("table", class_="data-table")
+
+            headers = [
+                th.get_text(strip=True)
+                for th in table.find("thead").find_all("th")[:]
+                ]
+
+            header_tag = headers[0]
+            headers = headers[1:]
+
+            data["metric"] = header_tag
+
+            for row in table.find("tbody").find_all("tr"):
+                label_cell = row.find("th")
+                label = label_cell.get_text(strip=True)
+                values = [
+                    td.get_text(strip=True)
+                    for td in row.find_all("td")
+                ]
+                data[label] = dict(zip(headers, values))
+
+            performance["load_characteristics"] = data
+            
+        if "performance curves" in h3_tags:
+            performance_curves_element = performance_div.find("div",class_="section drawings")
+
+            links = [a["href"] for a in performance_curves_element.find_all("a", href=True)]
+            
+            data["performance_curves"] = links
+            
+        if not h3_tags:
+            self.logger.info("No sessions found in Performance, fallback extraction will be used")
+            links = [a["href"] for a in performance_div.find_all("a", href=True)]
+            
+            performance["associated_urls"] = links
+            
         self.logger.info("Sucessfully retrieved performance info")
+        
         return performance
     
     def parse_parts(self,soup):
@@ -233,9 +306,11 @@ def main():
     
     links = [
         #"https://www.baldor.com/catalog/027603",
-        "https://www.baldor.com/catalog/1021W",
-        #"https://www.baldor.com/catalog/CD1803R",
-        #"https://www.baldor.com/catalog/BSM100C-1150AA"
+        #"https://www.baldor.com/catalog/1021W",
+        "https://www.baldor.com/catalog/CD1803R",
+        #"https://www.baldor.com/catalog/BSM100C-1150AA",
+        #"https://www.baldor.com/catalog/CD3433"
+        
     ]
     
     for url in links[:]:
