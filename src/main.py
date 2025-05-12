e558a2ccee9307d3e877cb31bf9183af3fb4e922
lrs50 <lucaspook12@gmail.com>
2025-05-12 12:24:44 -0300

    add: Comments and path return

--- src/main.py
+++ src/main.py
@@ -5,16 +5,24 @@ from pprint import pprint
 def main():
     
     links = [
-        "https://www.baldor.com/catalog/1021W"
-        ]
+        "https://www.baldor.com/catalog/027603",
+        "https://www.baldor.com/catalog/1021W",
+        # "https://www.baldor.com/catalog/CD1803R",
+        # "https://www.baldor.com/catalog/BSM100C-1150AA",
+        # "https://www.baldor.com/catalog/CD3433",
+        # "https://www.baldor.com/catalog/024018",
+        # "https://www.baldor.com/catalog/027550"
+        
+    ]
 
-    parser = Parser()
-    downloader = Downloader()
+    for link in links:
+        parser = Parser()
+        downloader = Downloader()
+        
+        raw_data = parser.run(link)
+        assets = downloader.run(raw_data)
+        
+        pprint(assets)
     
-    raw_data = parser.run(links[0])
-    downloader.run(raw_data)
-    
-    
-
 if __name__ == "__main__":
     main()
--- src/scraper/downloader.py
+++ src/scraper/downloader.py
@@ -1,15 +1,13 @@
 from typing import List, Dict, Any
-from bs4 import BeautifulSoup
-import re
-import json
 from pathlib import Path
 from urllib.parse import quote
-import sys
-import os
-from pathlib import Path
-from pprint import pprint
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# from pathlib import Path
+# from pprint import pprint
+
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from utils import *
 
@@ -40,32 +38,75 @@ def build_cad_url(value: str, original_url: str) -> str:
     encoded_url = quote(original_url, safe="")
     return f"https://www.baldor.com/api/products/download/?value={value}&url={encoded_url}"
 
+def unwrap(l:List[Any]) -> Any :
+    
+    return l[0] if len(l) == 1 else l
+
 class Downloader(object):
     def __init__(self,log_to_console: bool = True, log_to_file: bool = False):
+        """
+        Initializes the Downloader instance with logging and a resilient HTTP session.
+
+        Args:
+            log_to_console (bool): If True, logs will be printed to the console.
+            log_to_file (bool): If True, logs will be saved to a log file.
+        """
         
-        self.logger = get_logger("Parser", to_console=log_to_console, to_file=log_to_file)
+        self.logger = get_logger("Downloader", to_console=log_to_console, to_file=log_to_file)
         attach_urllib3_to_logger(self.logger)
         self.session = create_resilient_session()
         
         
-    def run(self,json: Dict[str, Any]):
-        
+    def run(self, json: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Orchestrates the download of all relevant assets for a given product.
+
+        Args:
+            json (Dict[str, Any]): Dictionary with all parsed product metadata.
+
+        Returns:
+            Dict[str, Any]: A dictionary with asset types as keys and their relative file paths as values.
+        """
         self.logger.info(f"{'_'*20} Started the Downloader for item {json['product_id']} {'_'*20}")
         
         self.json = json
         
         self.path = f"output/assets/{self.json['product_id']}"
+        self.relative_path = f"assets/{self.json['product_id']}"
         
-        self.fetch_and_save_drawings()
+        assets = {}
+        
+        assets["image"] = unwrap(self.download_main_image())
+        assets["manual"] = unwrap(self.download_product_manual())
+        assets["performance"] = unwrap(self.download_performance())
+        assets.update(self.download_drawings())
+        
+        return assets
     
     def download_file(self,url: str, destination: str) -> None:
+        """
+        Downloads a file from the given URL and saves it to the specified destination path.
+
+        Args:
+            url (str): The full URL of the file to download.
+            destination (str): The file path where the downloaded file will be saved.
+
+        Returns:
+            None
+        """ 
         
         try:
-            response = self.session.get(url,headers=DEFAULT_HEADERS,stream=True, timeout=10)
+            
+            if destination.endswith(".pdf"):
+                timeout = 60
+            else:
+                timeout = 10
+            
+            response = self.session.get(url,headers=DEFAULT_HEADERS,stream=True, timeout=timeout)
             response.raise_for_status()
         except Exception as e:
             self.logger.error(f"[UNEXPECTED ERROR] during request: {e}")
-            return {"results": {"matches": []}}
+            return None
         
         Path(destination).parent.mkdir(parents=True, exist_ok=True)
         
@@ -73,34 +114,61 @@ class Downloader(object):
             for chunk in response.iter_content(chunk_size=8192):
                 f.write(chunk)
         
-    def fetch_and_save_image(self):
+        self.logger.info(f"Successfully Downloaded {destination.split('/')[-1]}")        
+        return None
         
+    def download_main_image(self) -> List[str]:
+        """
+        Downloads the main product image based on the 'img_src' field in the JSON.
+
+        Returns:
+            List[str]: A list containing the relative path to the saved image file,
+                    or an empty list if no image was found.
+        """
         img_src = self.json.get("img_src")
         
         if img_src:
             img_url = build_image_url(img_src)
         else:
             self.logger.warning("No Main Image Source Found!")
+            return []
         
         self.download_file(img_url,f"{self.path}/img.jpg")
     
-    def fetch_and_save_pdf(self):
-        
+        return [f"{self.relative_path}/img.jpg"]
+
+    def download_product_manual(self) -> List[str]:
+        """
+        Downloads the product manual PDF from the 'pdf_src' field in the JSON.
+
+        Returns:
+            List[str]: A list containing the relative path to the saved manual file,
+                    or an empty list if not available.
+        """
         pdf_src = self.json.get("pdf_src")
         
         if pdf_src:
             pdf_url = build_product_file_url(pdf_src)
         else:
             self.logger.warning("No Product File Source Found!")
+            return []
         
         self.download_file(pdf_url,f"{self.path}/manual.pdf")
+
+        return [f"{self.relative_path}/manual.pdf"]
       
-    def fetch_and_save_performance(self):
-        
+    def download_performance(self) -> List[str]:
+        """
+        Downloads performance-related PDF files listed in the JSON under the 'performance' key.
+
+        Returns:
+            List[str]: A list of relative paths to the saved performance files.
+        """
         performance_dict = self.json.get("performance")
         
         if not performance_dict:
             self.logger.warning("No information about Performance!")
+            return []
         
         performance_curves_src = performance_dict.get("performance_curves",[])
         associated_urls = performance_dict.get("associated_urls",[])
@@ -109,14 +177,28 @@ class Downloader(object):
         
         if not associated_urls:
             self.logger.warning("No Associated urls in Performance")
-            return
+            return []
             
-        for i,url in enumerate(associated_urls):
-            self.download_file(url,f"{self.path}/performance_{i}.pdf")
-            
-    
-    def fetch_and_save_drawings(self):
         
+        paths = []    
+        
+        for i,url in enumerate(associated_urls):
+            self.download_file(url,f"{self.path}/performance_curve_{i}.pdf")
+            paths.append(f"{self.relative_path}/performance_curve_{i}.pdf")
+
+        return paths
+        
+    def download_drawings(self) -> Dict[str, Any]:
+        """
+        Downloads drawing-related files from the product JSON, including render images and CAD files.
+
+        If no drawing information is found, logs appropriate warnings and returns empty structures.
+
+        Returns:
+            Dict[str, Any]: A dictionary with keys:
+                - 'renders': List of relative paths to render files (or single string if only one)
+                - 'cads': List of relative paths to CAD files (or single string if only one)
+        """
         drawings_dict = self.json.get("drawings")
         
         if not drawings_dict:
@@ -131,15 +213,22 @@ class Downloader(object):
         if not cad_list:
             self.logger.warning("No Cads in Drawings!")
             
+        paths = {"renders":[],"cads":[]}  
         
         for i,img_dict in enumerate(img_list):
             url = build_drawing_img_url(self.json['product_id'],img_dict["number"])
-            self.download_file(url,f"{self.path}/drawing_img_{i}.pdf")
+            self.download_file(url,f"{self.path}/render_{i}.pdf")
+            paths["renders"].append(f"{self.relative_path}/render_{i}.pdf")
             
         for i,cad_dict in enumerate(cad_list):
             url = build_cad_url(cad_dict["value"],cad_dict["url"])
             
             file_type = cad_dict["value"].split(".")[-1]
-            name = f"cad_{cad_dict['filetype']}_{i}.{file_type}"
-            
+            name = f"cad_{cad_dict['filetype'].lower()}_{i}.{file_type}"
             self.download_file(url,f"{self.path}/{name}")
+            paths["cads"].append(f"{self.relative_path}/{name}")
+            
+        paths["renders"] = unwrap(paths["renders"])    
+        paths["cads"] = unwrap(paths["cads"])   
+        
+        return paths
